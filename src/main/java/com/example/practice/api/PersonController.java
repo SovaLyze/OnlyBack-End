1f757a14950d8292a255dc55d9b533a24896ae0a
Sova <yuralakey22@gmail.com>
2020-10-19 02:58:09 +0200

    Back-END Practice

---
+++ src/main/java/com/example/practice/api/PersonController.java
@@ -0,0 +1,49 @@
+package com.example.practice.api;
+
+import com.example.practice.model.Person;
+import com.example.practice.service.PersonService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.lang.NonNull;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.UUID;
+
+@RequestMapping("api/v1/person")
+@RestController
+public class PersonController {
+
+    private final PersonService personService;
+
+    @Autowired
+    public PersonController(PersonService personService) {
+        this.personService = personService;
+    }
+
+    @PostMapping
+    public void addPerson(@Validated @NonNull @RequestBody Person person) {
+        personService.addPerson(person);
+    }
+
+    @GetMapping
+    public List<Person> getAllPeople() {
+        return personService.getAllPeople();
+    }
+
+    @GetMapping(path = "{id}")
+    public Person getPersonId(@PathVariable("id") UUID id) {
+        return personService.getPersonById(id)
+                .orElse(null);
+    }
+
+    @DeleteMapping(path = {"id"})
+    public void deletePersonById(@PathVariable("id") UUID id) {
+        personService.deletePerson(id);
+    }
+
+    @PutMapping
+    public void updatePerson(@PathVariable("id") UUID id, @Validated @NonNull @RequestBody Person personToUpdate) {
+        personService.updatePerson(id, personToUpdate);
+    }
+}
